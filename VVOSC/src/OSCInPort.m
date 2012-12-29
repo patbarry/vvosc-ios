398895e719809e6148f0efa225eec5ff7595456d
Patrick Barry <github@patbarry.com>
2012-12-29 00:14:30 -0800

    possible to create LGPlayer objects manually instead of using autodiscovery .. also did some refactoring, and changed the mDNS service name for OSCInPorts created, so that they're not the same as player boards' service names

--- VVOSC/src/OSCInPort.m
+++ VVOSC/src/OSCInPort.m
@@ -178,7 +178,7 @@
 		}
 		zeroConfDest = [[NSNetService alloc]
 			initWithDomain:@"local."
-			type:@"_lumigeek-player._tcp."
+			type:@"_lumigeek-iosapp._tcp."
 			name:[NSString stringWithFormat:@"%@ %@",[[UIDevice currentDevice] name],portLabel]
 			port:port];
 		[zeroConfDest publish];
